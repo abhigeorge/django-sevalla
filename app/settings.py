925a49d8cc3314178845d20b67627aaab95aab5f
abhigeorge <abhigeorgekalladayil@gmail.com>
2025-11-04 23:48:17 +0530

    docker

--- app/settings.py
+++ app/settings.py
@@ -21,14 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-qft4#n0pdjfsl*wop4+7=v$c0idq&xhtlmnj)+q_%ya&s&o%t^'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['itsmyshot.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "itsmyshot.com").split(",")
 
-CSRF_TRUSTED_ORIGINS = ['https://itsmyshot.com']
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
 
 # Application definition
 
--- scripts/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
